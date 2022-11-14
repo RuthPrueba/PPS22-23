cc2243a50d8f094df7c7d4b0f4a6f1e5a8fe1aff
ChabierLo <96429155+ChabierLo@users.noreply.github.com>
2022-11-14 20:06:22 +0100

    Add files via upload

---
+++ Javier/factorialIterativo.py
@@ -0,0 +1,21 @@
+#######################################
+# calcula un factorial con un bucle
+######################################
+
+numero = int(input("introduce un número entero para calcular el factorial "))
+
+if numero < 0:
+	print("el número tiene que ser positivo")
+else:
+	
+	valor = numero
+	num = numero
+	while num >= 2:
+		num = num - 1
+		valor = valor * num 
+		
+	print("El factorial de "+ str(numero) + " es " + str(valor))
+
+
+
+
